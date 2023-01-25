880d80fd2a50331f0e3cb5eb04c4d3e53d7b2ad6
bin-hussin <39175651+bin-hussin@users.noreply.github.com>
2023-01-26 01:50:36 +0300

    try

--- myProject/admin/includes/templates/footer.php
+++
@@ -1,8 +0,0 @@
-<div class="footer">
-<script src="<?php echo $js ?>jquery.min.js"></script>
-<script src="<?php echo $js ?>bootstrap.min.js"></script>
-<script src="<?php echo $js ?>backend.js"></script>
-
-</div>
-</body>
-</html> 
